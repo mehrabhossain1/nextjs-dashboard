07c299618ef89b715bd810f89bdf0791b927c254
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-03-16 23:23:35 +0600

    git conflicts resolved

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,5 @@
 import '@/app/ui/global.css';
-<<<<<<< HEAD
-import { inter } from '@/app/ui/fonts';
-=======
 import { inter } from './ui/fonts';
->>>>>>> 0e763818c8e2e80f6973088ab52b44c250a41277
 
 export default function RootLayout({
   children,
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,3 @@
-// import { Inter } from 'next/font/google';
-
-// export const inter = Inter({ subsets: ['latin'] });
-
 import { Inter, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
